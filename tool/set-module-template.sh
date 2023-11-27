7960bf9f64bcbf6f070df058fa9e96113475974e
Richeve S. Bebedor <richeve.bebedor@gmail.com>
2023-11-27 23:04:09 +0800

    Added file check for shasum.

--- tool/set-module-template.sh
+++ tool/set-module-template.sh
@@ -4,8 +4,15 @@ set-template( ){
 	local MODULE_FILE_PATH="$1";
 	local TEMPLATE_FILE_PATH="$MODULE_FILE_PATH.template";
 
-	local MODULE_FILE_SHASUM="$(sha256sum "$MODULE_FILE_PATH" | cut -d ' ' -f 1)";
-	local TEMPLATE_FILE_SHASUM="$(sha256sum "$TEMPLATE_FILE_PATH" | cut -d ' ' -f 1)";
+	local MODULE_FILE_SHASUM="";
+
+	[[ -f "$MODULE_FILE_PATH" ]] && \
+	MODULE_FILE_SHASUM="$(sha256sum "$MODULE_FILE_PATH" | cut -d ' ' -f 1)";
+
+	local TEMPLATE_FILE_SHASUM="";
+
+	[[ -f "$TEMPLATE_FILE_PATH" ]] && \
+	TEMPLATE_FILE_SHASUM="$(sha256sum "$TEMPLATE_FILE_PATH" | cut -d ' ' -f 1)";
 
 	[[ "$MODULE_FILE_SHASUM" != "$TEMPLATE_FILE_SHASUM" ]] &&	\
 	[[ -f "$MODULE_FILE_PATH" ]] &&								\
